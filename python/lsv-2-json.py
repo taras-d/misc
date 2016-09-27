64d44a369b7a1965ab29f6d3c60779ec422bb81d
taras-d <taras.datsenko@gmail.com>
2016-09-27 10:22:04 +0300

    lsv 2 json

--- python/lsv-2-json.py
+++ python/lsv-2-json.py
@@ -1,14 +1,12 @@
 
 # Convert line separated values file to json file
-def line_sep_values_to_json(src_file, des_file):
+def lsv_2_json(src_file, des_file):
 
 	import json
 
 	src = open(src_file)
 	des = open(des_file, 'w')
 
-	data = [{ 'text': i.rstrip() } for i in src.readlines()]
+	data = [line.rstrip() for line in src.readlines()]
 	des.write(json.dumps(data, indent=4))
 
-line_sep_values_to_json('fns', 'job-functions.json')
-line_sep_values_to_json('inds', 'industries.json')
