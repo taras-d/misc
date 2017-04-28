3bec0c76c53e665c48bbd90378345654c96ed144
taras-d <taras.datsenko@gmail.com>
2017-04-28 16:03:29 +0300

    SignalR client example

---
+++ signalr/EchoHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WebApp.SignalR
+{
+    public class EchoHub : Hub
+    {
+        public string Ping()
+        {
+            Clients.All.newMessage("Today is " + new DateTime().DayOfWeek.ToString());
+
+            return "Pong";
+        }
+    }
+}
